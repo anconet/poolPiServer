1ddf3896d191267ab70092f1f5d9b685477e40de
Anthony Anconetani <anthony.anconetani@gmail.com>
2016-12-28 17:30:18 -0600

    Saw a JQuery text string go from the client to the server.

--- main.php
+++ main.php
@@ -18,8 +18,10 @@
 		</script>
 
 		<script>
-		//$.post("buttons.php",{"buttonPressed":"poolMode"},function(data){alert ("Data Loaded"+data)});
-		$.post("buttons.php",function(data){alert ("Data Loaded"+data)});
+		//$.post("buttons.php",{buttonPressed:"poolMode"});
+		$.post("buttons.php",{buttonPressed:"poolMode"},function(data){alert ("Data Loaded"+data)},"json");
+		//$.post("buttons.php","{buttonPressed:"poolMode"}",function(data){alert ("Data Loaded"+data)});
+		//$.post("buttons.php",function(data){alert ("Data Loaded"+data)});
 		//document.write("Did I get here?");
 		</script>
 
